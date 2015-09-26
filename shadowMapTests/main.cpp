318ace67a8629622e4876ec3ae8835fc2757441a
irajgreenberg <irajgreenberg@gmail.com>
2015-09-26 10:46:20 -0500

    shadows

--- shadowMapTests/main.cpp
+++ shadowMapTests/main.cpp
@@ -1,3 +1,4 @@
+#define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
@@ -11,6 +12,9 @@
 
 #include <iostream>
 #include <fstream>
+
+#include "ProtoUtility.h"
+#include "ProtoShader.h"
 
 // lazy
 using namespace std;
@@ -27,9 +31,9 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 
 
 // load shader
-unsigned long getFileLength(ifstream& file);
-int loadshader(char* filename, GLchar** ShaderSource, unsigned long* len);
-int unloadshader(GLubyte** ShaderSource);
+//unsigned long getFileLength(ifstream& file);
+//int loadshader(char* filename, GLchar** ShaderSource, unsigned long* len);
+//int unloadshader(GLubyte** ShaderSource);
 
 
 
@@ -43,6 +47,25 @@ int main(void)
 		//...
 	}
 	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION)); 
+
+	//ijg::ProtoShader shader("shader.vert", "shader.frag");
+
+	std::ifstream file("../shader.vert");
+	std::string line, block;
+	if (file.is_open()) {
+
+		while (!file.eof()) {
+			std::getline(file, line);
+			block.append(line);
+			block.append(std::string("\n"));
+		}
+		std::cout << block;
+		file.close();
+	}
+	else {
+		std::cout << "Unable to open file";
+	}
+
 	
 	GLFWwindow* window;
 	glfwSetErrorCallback(error_callback);
@@ -88,55 +111,55 @@ int main(void)
 }
 
 
-
-unsigned long getFileLength(ifstream& file)
-{
-	if (!file.good()) return 0;
-
-	unsigned long pos = file.tellg();
-	file.seekg(0, ios::end);
-	unsigned long len = file.tellg();
-	file.seekg(ios::beg);
-
-	return len;
-}
-
-int loadshader(char* filename, GLchar** ShaderSource, unsigned long* len)
-{
-	ifstream file;
-	file.open(filename, ios::in); // opens as ASCII!
-	if (!file) return -1;
-
-	len = getFileLength(file);
-
-	if (len == 0) return -2;   // Error: Empty File 
-
-	*ShaderSource = (GLubyte*) new char[len + 1];
-	if (*ShaderSource == 0) return -3;   // can't reserve memory
-
-	// len isn't always strlen cause some characters are stripped in ascii read...
-	// it is important to 0-terminate the real length later, len is just max possible value... 
-	*ShaderSource[len] = 0;
-
-	unsigned int i = 0;
-	while (file.good())
-	{
-		*ShaderSource[i] = file.get();       // get character from file.
-		if (!file.eof())
-			i++;
-	}
-
-	*ShaderSource[i] = 0;  // 0-terminate it at the correct position
-
-	file.close();
-
-	return 0; // No Error
-}
-
-
-int unloadshader(GLubyte** ShaderSource)
-{
-	if (*ShaderSource != 0)
-		delete[] * ShaderSource;
-	*ShaderSource = 0;
-}
+//
+//unsigned long getFileLength(ifstream& file)
+//{
+//	if (!file.good()) return 0;
+//
+//	unsigned long pos = file.tellg();
+//	file.seekg(0, ios::end);
+//	unsigned long len = file.tellg();
+//	file.seekg(ios::beg);
+//
+//	return len;
+//}
+//
+//int loadshader(char* filename, GLchar** ShaderSource, unsigned long* len)
+//{
+//	ifstream file;
+//	file.open(filename, ios::in); // opens as ASCII!
+//	if (!file) return -1;
+//
+//	len = getFileLength(file);
+//
+//	if (len == 0) return -2;   // Error: Empty File 
+//
+//	*ShaderSource = (GLubyte*) new char[len + 1];
+//	if (*ShaderSource == 0) return -3;   // can't reserve memory
+//
+//	// len isn't always strlen cause some characters are stripped in ascii read...
+//	// it is important to 0-terminate the real length later, len is just max possible value... 
+//	*ShaderSource[len] = 0;
+//
+//	unsigned int i = 0;
+//	while (file.good())
+//	{
+//		*ShaderSource[i] = file.get();       // get character from file.
+//		if (!file.eof())
+//			i++;
+//	}
+//
+//	*ShaderSource[i] = 0;  // 0-terminate it at the correct position
+//
+//	file.close();
+//
+//	return 0; // No Error
+//}
+//
+//
+//int unloadshader(GLubyte** ShaderSource)
+//{
+//	if (*ShaderSource != 0)
+//		delete[] * ShaderSource;
+//	*ShaderSource = 0;
+//}
